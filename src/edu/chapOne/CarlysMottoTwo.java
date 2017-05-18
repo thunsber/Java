87ae1af56fefb05bdddd389b8b01604f71157fca
Trevor <trevor.hunsb@gmail.com>
2017-05-18 12:16:11 -0400

    Chapter 1 Java exercises

---
+++ src/edu/chapOne/CarlysMottoTwo.java
@@ -0,0 +1,11 @@
+package edu.chapOne;
+
+public class CarlysMottoTwo {
+	public static void main(String[] args) {
+		System.out.println("*************************************************");
+		System.out.println("*                                               *");
+		System.out.println("* Carly's makes the food that makes it a party. *");
+		System.out.println("*                                               *");
+		System.out.println("*************************************************");
+	}
+}
---
+++ src/edu/chapOne/MovieQuoteInfo.java
@@ -0,0 +1,8 @@
+package edu.chapOne;
+
+public class MovieQuoteInfo {
+	public static void main(String[] args){
+		System.out.println("Does this mean we're not friends anymore?");
+	}
+
+}
